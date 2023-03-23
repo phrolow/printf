4768fd0952433a95efacd0426c86452ab8ae5956
phrolow <sheludcko.bogdan@gmail.com>
2023-03-23 23:24:47 +0300

    Default case - it just outputs percent, as it was

--- printf.asm
+++ printf.asm
@@ -21,9 +21,3 @@ section .text
 
 section .data
     msg db "ochko %kirilla", 0xa, 0x0
-    
-call_table:
-    dw case_default
-    dw case_default
-    dw case_default
-    dw 'x' - 'd' - 1 dup(case_default)
